d4c953bbeddcb2a2b4ec552d8f1bad8c8cd1a056
j-w-matlock <matlock.john.w@icloud.com>
2025-09-11 13:12:50 -0700

    Improve UI and rebuild water demand tab

---
+++ Views/WaterDemandView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EconToolbox.Desktop.Views
+{
+    public partial class WaterDemandView : UserControl
+    {
+        public WaterDemandView()
+        {
+            InitializeComponent();
+        }
+    }
+}
