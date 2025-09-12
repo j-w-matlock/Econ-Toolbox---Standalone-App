159c9bd5377e39d8c75e63ac11382b769a23cc56
j-w-matlock <matlock.john.w@icloud.com>
2025-09-12 15:52:43 -0700

    Streamline Water Demand inputs

--- Models/Scenario.cs
+++ Models/Scenario.cs
@@ -16,16 +16,18 @@ namespace EconToolbox.Desktop.Models
         public double BasePerCapitaDemand { get; set; }
         public double PopulationGrowthRate { get; set; }
         public double PerCapitaDemandChangeRate { get; set; }
-        public double StandardGrowthRate { get; set; }
 
-        public double CurrentIndustrialPercent { get; set; }
-        public double FutureIndustrialPercent { get; set; }
-        public double CurrentResidentialPercent { get; set; }
-        public double FutureResidentialPercent { get; set; }
-        public double CurrentCommercialPercent { get; set; }
-        public double FutureCommercialPercent { get; set; }
-        public double CurrentAgriculturalPercent { get; set; }
-        public double FutureAgriculturalPercent { get; set; }
+        /// <summary>
+        /// Percentage shares for each demand sector. The last entry is treated
+        /// as the residual category that is auto-calculated to total 100%.
+        /// </summary>
+        public ObservableCollection<SectorShare> Sectors { get; } = new()
+        {
+            new SectorShare { Name = "Industrial" },
+            new SectorShare { Name = "Residential" },
+            new SectorShare { Name = "Commercial" },
+            new SectorShare { Name = "Agricultural", IsResidual = true }
+        };
         public double SystemImprovementsPercent { get; set; }
         public double SystemLossesPercent { get; set; }
 
