ebbae570af862cfbf273edb3178c127a4ac6b638
j-w-matlock <matlock.john.w@icloud.com>
2025-09-18 16:22:02 -0700

    Finalize interface refinements and new modules

---
+++ Views/GanttView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EconToolbox.Desktop.Views
+{
+    public partial class GanttView : UserControl
+    {
+        public GanttView()
+        {
+            InitializeComponent();
+        }
+    }
+}
