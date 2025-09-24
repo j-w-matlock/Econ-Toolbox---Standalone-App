2000a83b535c2d1a9b2d082d9686320ae3e79518
j-w-matlock <matlock.john.w@icloud.com>
2025-09-24 10:13:59 -0700

    Add agriculture depth-damage module with Monte Carlo visualization

---
+++ Views/AgricultureDepthDamageView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace EconToolbox.Desktop.Views
+{
+    public partial class AgricultureDepthDamageView : UserControl
+    {
+        public AgricultureDepthDamageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
